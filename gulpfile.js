643259968c59af3b38b4cd63f691eab0664a08ed
Bill Hogan <hogan.wp@gmail.com>
2016-02-07 10:59:58 -0800

    Add js prod

--- gulpfile.js
+++ gulpfile.js
@@ -158,13 +158,20 @@ gulp.task('images', function() {
 });
 
 //
-// Concatenate & Minify JS -- gulp js
+// Concatenate & Minify non-min JS files
 //
 gulp.task('js', function() {
-  return gulp.src(theme_dir + '/js/*.js')
+  return gulp.src([theme_dir + '/js/*.js', '!' + theme_dir + '/js/*min.js'])
     .pipe(plugins.concat('main.js'))
     .pipe(plugins.rename({suffix: '.min'}))
     .pipe(plugins.uglify())
+    .pipe(gulp.dest(theme_dir + '/js'));
+});
+
+//production js -- minifies all non min js, concats into single file
+gulp.task('jsp', ['js'], function() {
+  return gulp.src(theme_dir + '/js/*min.js')
+    .pipe(plugins.concat('mini2.js'))
     .pipe(gulp.dest(theme_dir + '/js_test'));
 });
 
