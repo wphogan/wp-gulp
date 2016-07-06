6e2a9a01b527e575490de8bd62b3437c6db82890
Bill Hogan <hogan.wp@gmail.com>
2016-07-05 21:56:30 -0700

    clean up unused tasks

--- gulpfile.js
+++ gulpfile.js
@@ -12,12 +12,18 @@ function getTask(task) {
     return require('./tasks/' + task)(gulp, plugins, theme_dir, sass_dir, css_dir);
 }
 
-gulp.task('all-wp-images', getTask('gulp-all-wp-images.js'));
+// Gulp tasks:
+
+// gulp images
 gulp.task('images', getTask('gulp-images.js'));
+
+// gulp js
 gulp.task('js', getTask('gulp-js.js'));
-gulp.task('jsp', getTask('gulp-jsp.js'));
-gulp.task('production', getTask('gulp-production.js'));
+
+// gulp styles
 gulp.task('styles', getTask('gulp-styles.js'));
+
+// gulp watch 
 gulp.task('watch', getTask('gulp-watch.js'));
 
 // Gulp default task, watches changes in SASS and compresses images -- runs with 'gulp'
