920e7c04e68eae0c292b17446519466f37bab804
william <william@designacion.org>
2016-07-19 10:22:35 -0700

    no more comb in styles!

--- tasks/gulp-styles.js
+++ tasks/gulp-styles.js
@@ -6,12 +6,6 @@ module.exports = function (gulp, plugins, theme_dir, sass_dir, css_dir) {
     return function () {
         var onError = require('./error.js').onError;
         return gulp.src(sass_dir + "*.+(scss|sass)")
-          .pipe(plugins.csscomb('./config/csscomb_config.json'))
-          .on("error", plugins.notify.onError({
-              message: "Oh shit, error on line: <%= error.line %> ",
-              title: "Even coding rock-stars make mistakes"
-            }))
-          .pipe(gulp.dest(sass_dir))
           .pipe(plugins.sourcemaps.init())
           .pipe(plugins.sass(function () {
             this.emit("error", new Error("Something happend: Sass crashed!"))}))
@@ -20,7 +14,7 @@ module.exports = function (gulp, plugins, theme_dir, sass_dir, css_dir) {
               title: "Even coding rock-stars make mistakes"
             }))
           .pipe(plugins.plumber({errorHandler: onError}))
-          .pipe(plugins.autoprefixer("last 3 version","safari 5", "ie 8", "ie 9", "ie 10", "ie 11")) //'Android >= 4','Chrome >= 20','Firefox >= 24', // Firefox 24 is the latest ESR
+          .pipe(plugins.autoprefixer("last 3 version","safari 5", "ie 8", "ie 9", "ie 10", "ie 11"))
           .pipe(plugins.minifyCss())
           .pipe(plugins.sourcemaps.write()) // for external file add ('../maps')
           .pipe(gulp.dest(css_dir))
